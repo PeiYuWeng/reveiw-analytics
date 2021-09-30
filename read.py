0a2d207973457ce1de147f324205d9ebf8ff4dd2
PeiYu Weng <eva.wengpeiyu@gmail.com>
2021-09-30 15:18:24 +0800

    add filter

--- read.py
+++ read.py
@@ -13,3 +13,16 @@ sum_len = 0
 for d in data:
     sum_len += len(d)
 print('留言的平均長度為',sum_len / len(data))
+
+#篩選
+new = []
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+
+good = []
+for d in data:
+    if 'good' in d:
+        good.append(d)
+print('一共有', len(good), '筆留言提到good')
